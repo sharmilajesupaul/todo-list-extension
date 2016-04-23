3d92a1ce46ceb7b7c374a9227c0ffa96a0f73fe9
Sharmila Jesupaul <tosharmila@gmail.com>
2016-04-23 14:02:15 -0700

    adds empty progress circle fill

--- popup.js
+++ popup.js
@@ -19,7 +19,37 @@ function Task() {
   this.updated = null;
 };
 
-document.addEventListener('DOMContentLoaded', function() {
+function createTask(task) {
+  // Test to see if the browser supports the HTML template element by checking
+  // for the presence of the template element's content attribute.
+  if ('content' in document.createElement('template')) {
+
+    // Instantiate the table with the existing HTML tbody and the row with the template
+    var t = document.querySelector('#taskRow'),
+      body = t.content.querySelector("span.list-item");
+    body.textContent = task.body;
+
+    // Clone the new row and insert it into the table
+    var tb = document.getElementsByTagName("tbody");
+    var clone = document.importNode(t.content, true);
+    tb[0].appendChild(clone);
+  }
+}
+
+function drawProgress(value) {
+  var value = value || 0;
+  $('#circle').circleProgress({
+    value: value,
+    size: 40,
+    emptyFill: "#ccc",
+    fill: {
+      gradient: [['#0681c4', .5], ['#4ac5f8', .5]],
+      gradientAngle: Math.PI / 4
+    }
+  });
+}
+
+$(document).ready(function() {
   $('body').css('height', '');
   $('#openTab').click(function(event) {
     chrome.tabs.create({ url: 'page.html' });
@@ -29,27 +59,6 @@ document.addEventListener('DOMContentLoaded', function() {
     $(this).toggleClass('fa-square-o').toggleClass('fa-check-square');
     $(this.parentNode).toggleClass('stroke-completed');
   });
+
+  drawProgress();
 });
-
-$('#circle').circleProgress({
-  value: 0.75,
-  size: 40,
-  fill: {
-    gradient: ["red", "orange"]
-  }
-});
-
-// Test to see if the browser supports the HTML template element by checking
-// for the presence of the template element's content attribute.
-if ('content' in document.createElement('template')) {
-
-  // Instantiate the table with the existing HTML tbody and the row with the template
-  var t = document.querySelector('#taskRow'),
-  body = t.content.querySelector("span.list-item");
-  body.textContent = "1235646565";
-
-  // Clone the new row and insert it into the table
-  var tb = document.getElementsByTagName("tbody");
-  var clone = document.importNode(t.content, true);
-  tb[0].appendChild(clone);
-}
